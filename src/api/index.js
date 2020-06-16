6763b2d1151956d38d3d459556547e0d9c60f8ac
Salman <salmanzafar1811@gmail.com>
2020-06-17 02:01:20 +0530

    Fetch data from api

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,12 @@ const url = "https://covid19.mathdro.id/api";
 
 export const fetchData = async () => {
   try {
-    const response = await axios.get(url);
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
 
-    return response;
+    const modiefiedData = { confirmed, recovered, deaths, lastUpdate };
+
+    return modiefiedData;
   } catch (error) {}
 };
