fd4c888b1afadab9ea2f262ab200b10f68ee411a
Salman <salmanzafar1811@gmail.com>
2020-06-18 01:45:59 +0530

    Add Chart with data

--- src/api/index.js
+++ src/api/index.js
@@ -13,3 +13,17 @@ export const fetchData = async () => {
     return modiefiedData;
   } catch (error) {}
 };
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    const modiefiedDailyData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+
+    return modiefiedDailyData;
+  } catch (error) {}
+};
