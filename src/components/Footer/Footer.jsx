95ea733be48db7de449fc3ebc67b0f51cefeab49
Salman <salmanzafar1811@gmail.com>
2020-06-19 03:18:37 +0530

    Update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return <div className={styles.copyright}>Made By Salman Zafar</div>;
+  return <div className={styles.copyright}>Copyright @ Salman Zafar</div>;
 };
 
 export default Footer;
